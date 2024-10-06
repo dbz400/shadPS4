e8ab685628ba27e062e553816a97910ff438186b
dbz400 <jacky.kk.tsui@gmail.com>
2024-10-07 00:39:05 +0800

    Update liverpool_to_vk.h

--- src/video_core/renderer_vulkan/liverpool_to_vk.h
+++ src/video_core/renderer_vulkan/liverpool_to_vk.h
@@ -75,8 +75,9 @@ static inline vk::Format PromoteFormatToDepth(vk::Format fmt) {
         return vk::Format::eD32Sfloat;
     } else if (fmt == vk::Format::eR16Unorm) {
         return vk::Format::eD16Unorm;
+    } else if (fmt == vk::Format::eR8G8B8A8Unorm) {
+        return vk::Format::eR8G8B8A8Unorm;
     }
-    UNREACHABLE();
 }
 
 } // namespace Vulkan::LiverpoolToVK
