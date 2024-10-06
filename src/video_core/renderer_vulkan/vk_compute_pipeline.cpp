1f2f84ba034c6065107fb7b9342798d6d525cc27
dbz400 <jacky.kk.tsui@gmail.com>
2024-10-07 00:20:40 +0800

    Update vk_compute_pipeline.cpp

--- src/video_core/renderer_vulkan/vk_compute_pipeline.cpp
+++ src/video_core/renderer_vulkan/vk_compute_pipeline.cpp
@@ -192,8 +192,6 @@ bool ComputePipeline::BindResources(VideoCore::BufferCache& buffer_cache,
             const auto [vk_buffer, offset] =
                 buffer_cache.ObtainBuffer(address, size, desc.is_written, true);
             const u32 fmt_stride = AmdGpu::NumBits(vsharp.GetDataFmt()) >> 3;
-            ASSERT_MSG(fmt_stride == vsharp.GetStride(),
-                       "Texel buffer stride must match format stride");
             const u32 offset_aligned = Common::AlignDown(offset, alignment);
             const u32 adjust = offset - offset_aligned;
             ASSERT(adjust % fmt_stride == 0);
